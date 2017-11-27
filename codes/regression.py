05d08f49b78fd8c6ec83eb903fb67a6146625746
shivajbd <shiva.verma@smartron.com>
2017-11-27 11:09:57 +0530

    regression modified

--- codes/regression.py
+++ codes/regression.py
@@ -1,23 +1,21 @@
+# author: Shiva Verma, Mail: shivajbd@gmail.com
+# linear model regression
+
 import numpy as np
 
 
-def predict(feature_data, feature_test, points):
+def predict(feature_data1, feature_data2, feature_test):
 
     from sklearn.linear_model import LinearRegression
     c = LinearRegression()
-    c.fit(feature_data, feature_test)
-    return c.predict(points)
-
-
-def accuracy(out, inp):
-
-    from sklearn.metrics import accuracy_score
-    return accuracy_score(out, inp)
+    c.fit(feature_data1, feature_data2)                # fitting data
+    print c.score(feature_data1, feature_data2)        # r-square score
+    return c.predict(feature_test)                     # predicting output
 
 
 if __name__ == '__main__':
 
-    x = np.array([[2], [4], [24], [6], [10]])
+    x = np.array([[3], [3], [21], [7], [10]])
     y = np.array([[1], [2], [12], [3], [5]])
     z = np.array([[2], [3]])
     pred = predict(x, y, z)
