c7334f9ba4de510419e22b1503ffd6b0d206bb2f
shivajbd <shiva.verma@smartron.com>
2017-11-28 10:11:31 +0530

    Project/outliers changed

--- Projects/outliers/enron_outliers.py
+++ Projects/outliers/enron_outliers.py
@@ -7,13 +7,30 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 
-### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+# read in data dictionary, convert to numpy array
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+
+name = ''
+print data_dict
+for i in data_dict:
+    if data_dict[i]['salary'] == 26704229:
+        name = i
+        break
+
+data_dict.pop(name)
+
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
-### your code below
+# your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
 
--- Projects/outliers/outlier_cleaner.py
+++ Projects/outliers/outlier_cleaner.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+import heapq
+
+
+def comp(self, other):
+
+    return cmp(self.error, other.error)
 
 
 def outlierCleaner(predictions, ages, net_worths):
@@ -6,15 +12,22 @@ def outlierCleaner(predictions, ages, net_worths):
         Clean away the 10% of points that have the largest
         residual errors (difference between the prediction
         and the actual net worth).
-
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+    h = []
+    for i in range(len(ages)):
+        error = -abs(predictions[i]-net_worths[i])
+        heapq.heappush(h, (error, ages[i], net_worths[i]))
+
+    for i in range(9):
+        heapq.heappop(h)      # removing outlier with higher error
+
     cleaned_data = []
 
-    ### your code goes here
+    for i in h:
+        tup = (i[1], i[2], i[0])
+        cleaned_data.append(tup)
 
-    
     return cleaned_data
 
