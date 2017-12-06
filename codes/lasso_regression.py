4976f5591979dac516134d6fb0244471a1ff8d7a
shivajbd <shiva.verma@smartron.com>
2017-12-06 17:55:27 +0530

    lasso regression added

---
+++ codes/lasso_regression.py
@@ -0,0 +1,20 @@
+# author: Shiva Verma, Mail: shivajbd@gmail.com
+# lasso regression
+
+import numpy as np
+
+
+def predict(feature_data1, feature_data2):
+
+    from sklearn.linear_model import Lasso
+    c = Lasso()
+    c.fit(feature_data1, feature_data2)             # fitting data
+    # print c.score(feature_data1, feature_data2)   # r-square score
+    print c.coef_                                   # coefficient of each feature
+
+
+if __name__ == '__main__':
+
+    x = np.array([[3, 2], [3, 1], [21, 5], [7, 21], [10, 32]])
+    y = np.array([[1], [2], [12], [3], [5]])
+    predict(x, y)
