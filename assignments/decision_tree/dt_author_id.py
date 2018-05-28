bf305884f351ad38b13a57bab155488bf39a64b9
shiva verma <shivajbd@gmail.com>
2018-05-28 20:02:09 +0530

    finding donors project added

---
+++ assignments/decision_tree/dt_author_id.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import sys
+from time import time
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+""" 
+    This is the code to accompany the Lesson 3 (decision tree) mini-project.
+    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Sara has label 0
+    Chris has label 1
+"""
+
+
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+len = len(features_train[1])
+print len
+
+clf = DecisionTreeClassifier(min_samples_split=40)
+
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+acc = accuracy_score(pred, labels_test)
+print acc
